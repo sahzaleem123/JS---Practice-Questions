2d7bb5df30fd2bdce0d09b9015f54616aeb74c6f
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-06-26 17:38:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//          No:01
+// for (let i = 0 ;i < 100; i++ ){
+//     console.log("clap");
+// }
+//          No:02
+// for (let i = 1 ;i <= 1000; i++ ){
+//     console.log(i);
+// }
+//          No:03
+// for (let i = 1000 ;i >= 1; i-- ){
+//     console.log(i);
+// }
+//         No:04
+// for (let i=1;i <= 100; i++ ){
+//     console.log(i+i)
+// }
+//          No:05
+// for (let i=1;i <= 100; i++ ){
+//     console.log(i*i)
+// }
+//          No:06
+// for (let i=1;i <= 100; i++ ){
+//     console.log(i*i)
+// }
+//          No:07
+// for (let i=1;i <= 100; i++ ){
+//     console.log(i*i*i)
+// }
+//          No:08
+// for (let i=1;i <= 99; i++ ){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+//          No:09
+// for(let i = 7; i <= 82; i++ ){
+//     if(i%2==0){
+//         let evenNum = [i];
+//         console.log(evenNum.length);
+//     }
+// }
+//          No:10
+// function Loops() {
+//     let firstNum = parseInt(document.getElementById("first").value);
+//     let lastNum = parseInt(document.getElementById("last").value);
+
+//     if(firstNum<lastNum){
+//         for (let i = firstNum ;i <= lastNum; i++ ){
+//             console.log(i);
+//         }
+//     }
+//     else{
+//         for (let i = firstNum ;i >= lastNum; i-- ){
+//             console.log(i);
+//         }
+//     }
+// }
+//             No:11
+function Table(){
+    let getTable = parseInt(document.getElementById("inp-value").value)
+    let timesTable = parseInt(document.getElementById("times-value").value)
+
+    for(let i = 1;i <=timesTable; i++ ){
+        console.log(getTable*i)
+    }
+}
