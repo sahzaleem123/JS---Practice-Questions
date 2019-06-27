8805aeb6e3e3a20e4d6a49151eb46dfa8c0c0e8c
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-06-27 17:20:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,45 +2,57 @@
 // for (let i = 0 ;i < 100; i++ ){
 //     console.log("clap");
 // }
+
 //          No:02
 // for (let i = 1 ;i <= 1000; i++ ){
 //     console.log(i);
 // }
+
 //          No:03
 // for (let i = 1000 ;i >= 1; i-- ){
 //     console.log(i);
 // }
+
 //         No:04
 // let sum = 0;
 // for (let i = 1; i <= 100; i++) {
 //     sum = sum + i;
 // }
 // console.log(sum);
+
 //          No:05
-// for (let i=1;i <= 100; i++ ){
-//     console.log(i*i)
+// let product = 1;
+// for (let i = 1; i <= 100; i++) {
+//     product = i * i;
 // }
+// console.log(product);
+
 //          No:06
 // for (let i=1;i <= 100; i++ ){
 //     console.log(i*i)
 // }
+
 //          No:07
 // for (let i=1;i <= 100; i++ ){
 //     console.log(i*i*i)
 // }
+
 //          No:08
 // for (let i=1;i <= 99; i++ ){
 //     if(i%2==0){
 //         console.log(i);
 //     }
 // }
+
 //          No:09
+// let a=0;
 // for(let i = 7; i <= 82; i++ ){
 //     if(i%2==0){
-//         let evenNum = [i];
-//         console.log(evenNum.length);
+//         a=a+1;
 //     }
 // }
+//     console.log(a);
+
 //          No:10
 // function Loops() {
 //     let firstNum = parseInt(document.getElementById("first").value);
